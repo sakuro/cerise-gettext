6bc942eb160c9302bab2b00754a1da0f4e0e3334
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-24 19:01:12 +0900

    $ bundle gem cerise-gettext

---
+++ lib/cerise/gettext/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Cerise
+  module Gettext
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/cerise/gettext_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cerise::Gettext do
+  it "has a version number" do
+    expect(Cerise::Gettext::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
